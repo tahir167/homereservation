3eb3ad4e3f14709b2f7b8631d5cef3e9a2ba34a6
tahir167 <tahirxedice2005@gmail.com>
2025-05-05 06:41:23 -0700

    first commit

---
+++ src/constants/constants.js
@@ -0,0 +1,11 @@
+export const API_BASE_URL = "http://localhost:3000";
+
+export const endpoints = {
+    users : "/users",
+    apartments: "/apartments",
+    bookings: "/bookings",
+    contacts: "/contacts",
+    coupons: "/coupons",
+    sliders: "/sliders",
+    reviews: "/reviews"
+};
