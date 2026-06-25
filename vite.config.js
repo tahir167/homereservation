74759d7dda3d809b5abc3f23e828375341b94074
tahir167 <tahirxedice2005@gmail.com>
2026-06-25 17:00:51 +0400

    updated vite config file

--- vite.config.js
+++ vite.config.js
@@ -8,17 +8,20 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
+        index: resolve(__dirname, "index.html"),
         about: resolve(__dirname, "about.html"),
         apartments: resolve(__dirname, "apartments.html"),
         apartmentsDetail: resolve(__dirname, "apartments.detail.html"),
+        book: resolve(__dirname, "book.html"),
         contact: resolve(__dirname, "contact.html"),
         login: resolve(__dirname, "login.html"),
+        person: resolve(__dirname, "person.html"),
         signup: resolve(__dirname, "signup.html"),
-        book: resolve(__dirname, "book.html"),
         adminUsers: resolve(__dirname, "admin.users.html"),
-        adminApartments: resolve(__dirname, "admin.apartments.html")
-      }
-    }
-  }
+        adminBooks: resolve(__dirname, "admin.books.html"),
+        adminStatistics: resolve(__dirname, "admin.statistics.html"),
+        adminAddApartments: resolve(__dirname, "admin.addaparments.html"),
+      },
+    },
+  },
 });
